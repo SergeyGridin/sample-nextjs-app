46c87db0b284207e865c7a6b456b5fe0d394c52a
SergeyGridin <gridin21@gmail.com>
2020-11-07 15:29:48 -0500

    first page link

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function FirstPost() {
+  return (
+    <>
+      <h1>FirstPost</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to Home</a>
+        </Link>
+      </h2>
+    </>
+  );
+}
